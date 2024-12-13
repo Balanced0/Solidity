3a7610fea0995bab4ce7948e529e5e88cc53ab1a
MD. Alvi Chowdhury <167624337+Balanced0@users.noreply.github.com>
2024-12-13 12:12:14 +0600

    Basic data Types: Boolean

---
+++ contracts/Basic_data_types.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Basic_data_types {
+	bool public a;
+    bool public b;
+    constructor(){
+        a = true;
+        b = false;
+    }
+}
