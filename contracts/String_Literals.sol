3f8ff1474c14ec9ea45cf1b12483b1cdeafed5e4
MD. Alvi Chowdhury <167624337+Balanced0@users.noreply.github.com>
2024-12-18 12:58:37 +0600

    String_Literals

---
+++ contracts/String_Literals.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract String_Literals {
+	bytes32 public msg1 = "Hello World";
+    string public msg2 = "My name is MISTYCAN. I am the best programmer in the world!";
+}
